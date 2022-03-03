1443683fdcc05d5c414b873e735c9b0243f75f27
Kyle Escobar <kyleescobar@spectralpowered.org>
2022-03-02 22:45:35 -0500

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "1.6.10"
+}
+
+group = "dev.nxtscape"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,10 @@
+plugins {
+    // See https://jmfayard.github.io/refreshVersions
+    id("de.fayard.refreshVersions") version "0.40.1"
+}
+
+rootProject.name = "nxtscape"
+
+refreshVersions {
+    enableBuildSrcLibs()
+}
